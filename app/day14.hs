5b0e91c84d4d868a128bf1ca049f49b99e01b26e
Corey Knight <corey.knight@learnosity.com>
2018-01-01 00:00:00 +0000

    omfg still debugging day14 part2. my countRegions is broken because it won't correctly detect connections that come later.

--- app/day14.hs
+++ app/day14.hs
@@ -21,6 +21,7 @@ main = do
   putStrLn "-2"
   "flqrgnkx" & makeCodes & map knotHash & head & print
   putStrLn "-3"
+  "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & length & print
   "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & head & print
   putStrLn "-4"
   "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & concat & length & print
@@ -30,32 +31,58 @@ main = do
   "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & concat & binaryToBools & boolsToIndices & length & print
   "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & concat & binaryToBools & boolsToIndices & take 10 & print
   putStrLn "-7"
-  "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & concat & binaryToBools & boolsToIndices & indicesToRegions & Map.size & print
+  "flqrgnkx" & makeCodes & map knotHash & map hexToBinary & concat & binaryToBools & boolsToIndices & indicesToRegions 128 & Map.size & print
 
   -- part 1
   putStrLn "---"
-  "flqrgnkx" & keyToRegions & Map.keys & length & print
-  "jzgqcdpd" & keyToRegions & Map.keys & length & print
+  "flqrgnkx" & keyToBools & boolsToRegions 128 & Map.keys & length & print
+  "jzgqcdpd" & keyToBools & boolsToRegions 128 & Map.keys & length & print
+
+  -- part 2 sample
+  putStrLn "---"
+  let part2TestData1 = binaryToBools $
+                          "0111"
+                       ++ "0101"
+                       ++ "1010"
+                       ++ "1111"
+  let part2TestData2 rowLength = cycle [True, False] & take rowLength & map (\v -> iterate not v & take rowLength) & concat
+  let part2TestData3 = binaryToBools $
+                          "0000000"
+                       ++ "0111000"
+                       ++ "0101000"
+                       ++ "0000000"
+                       ++ "0001010"
+                       ++ "0001110"
+                       ++ "0000000"
+  let part2TestData4 = binaryToBools $
+                          "01"
+                       ++ "10"
+
+  putStr "expected: 2, actual: " >> part2TestData1 & boolsToRegions 4 & countRegions & print
+  part2TestData2 4 & boolsToRegions 4 & Map.showTree & putStrLn
+  part2TestData2 4 & boolsToIndices & print
+  putStr "expected: 8, actual: " >> part2TestData2 4 & boolsToRegions 4 & countRegions & print
+  putStr "expected: 2, actual: " >> part2TestData3 & boolsToRegions 7 & countRegions & print
+  putStr "expected: 2, actual: " >> part2TestData4 & boolsToRegions 2 & countRegions & print
 
   -- part 2
   putStrLn "---"
-  "flqrgnkx" & keyToRegions & countRegions & print
-  "jzgqcdpd" & keyToRegions & countRegions & print
+  "flqrgnkx" & keyToBools & boolsToRegions 128 & countRegions & print
+  "jzgqcdpd" & keyToBools & boolsToRegions 128 & countRegions & print
   putStrLn "(2122 is too high)"
 
-keyToRegions :: String -> Map Int Int
-keyToRegions
-  = indicesToRegions
-  . boolsToIndices
-  . binaryToBools
+keyToBools :: String -> [Bool]
+keyToBools
+  = binaryToBools
   . concat
   . map hexToBinary
   . map knotHash
   . makeCodes
 
---keyToGridIndices :: String -> [Int]
---keyToGridIndices input = input & makeCodes & makeHashes & makeBoolsGrid & getIndices
---  where makeHashes = map (hexToBinary . knotHash)
+boolsToRegions :: Int -> [Bool] -> Map Int Int
+boolsToRegions rowLength
+  = indicesToRegions rowLength
+  . boolsToIndices
 
 boolsToIndices :: [Bool] -> [Int]
 boolsToIndices values = zip [0..] values & filter snd & map fst
@@ -63,17 +90,18 @@ boolsToIndices values = zip [0..] values & filter snd & map fst
 binaryToBools :: String -> [Bool]
 binaryToBools = map (== '1')
 
-indicesToRegions :: [Int] -> Map Int Int
-indicesToRegions = foldl' (addPixel 128) Map.empty
+indicesToRegions :: Int -> [Int] -> Map Int Int
+indicesToRegions rowLength = foldl' (addPixel rowLength) Map.empty
 
 countRegions :: Map Int Int -> Int
 countRegions = Set.size . Set.fromList . Map.elems
 
+-- TODO: i hate this method; it would be great to refactor it to be much longer so that it's clear
 addPixel :: Int -> Map Int Int -> Int -> Map Int Int
 addPixel rowLength regions index = Map.insert index (head possibleValues) regions
   where possibleValues = catMaybes [
-          Map.lookup (index - 1) regions,
-          Map.lookup (index + 1) regions,
+          if index `mod` rowLength == 0 then Nothing else Map.lookup (index - 1) regions,
+          if index + 1 `mod` rowLength == 0 then Nothing else Map.lookup (index + 1) regions,
           Map.lookup (index - rowLength) regions,
           Map.lookup (index + rowLength) regions,
           Just index
