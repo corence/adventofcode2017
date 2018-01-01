bd901b7343ff3a5586cbdba281cda8b88c1a7132
Corey Knight <corey.knight@learnosity.com>
2018-01-01 00:00:00 +0000

    wewt, now it's fixed

--- src/Hex.hs
+++ src/Hex.hs
@@ -2,24 +2,65 @@
 module Hex where
 
 import qualified Data.Map as Map
-import Data.Map(Map, (!))
+import Data.Map(Map)
+import Lib
+
+import Debug.Trace
 
 type Pos = Map Dir Int
+
+data Dir = NW | N | NE | SE | S | SW deriving (Eq, Ord, Show)
+allDirs = [NW, N, NE, SE, S, SW]
 
 origin :: Pos
 origin = Map.empty
 
-data Dir = NE | N | NW | SE | S | SW
-           deriving (Eq, Ord)
+normalize :: Pos -> Pos
+normalize pos
+  = if pos == pos2 -- ewwww. but we need it because otherwise these operations are order-sensitive.
+      then pos2
+      else normalize pos2
+    where pos2 = pos
+            & transmute N NE NW
+            & transmute S SE SW
+            & transmute NE N SE
+            & transmute NW N SW
+            & transmute SE S NE
+            & transmute SW S NW
+            & cancel N S
+            & cancel NW SE
+            & cancel NE SW
+
+-- the given 2 directions cancel each other out
+-- one of the directions is guaranteed to be zero after this
+cancel :: Dir -> Dir -> Pos -> Pos
+cancel dir1 dir2 = commute [] [dir1, dir2]
+
+-- the "target" direction absorbs whatever shared value the two "source" directions have
+-- for example, 2 northeast + 2 south becomes 2 southeast
+transmute :: Dir -> Dir -> Dir -> Pos -> Pos
+transmute target source1 source2 = commute [target] [source1, source2]
+
+-- the "target" direction absorbs whatever shared value the two "source" directions have
+-- for example, 2 northeast + 2 south becomes 2 southeast
+commute :: [Dir] -> [Dir] -> Pos -> Pos
+commute targets sources pos
+  = pos
+  & (\pos -> foldr (sub minValue) pos sources)
+  & (\pos -> foldr (add minValue) pos targets)
+  where add amount dir = Map.insertWith (+) dir amount
+        sub amount = Map.update (Just . subtract amount)
+        minValue = map value sources & minimum
+        value dir = Map.findWithDefault 0 dir pos
+
+distanceFromOrigin :: Pos -> Int
+distanceFromOrigin = sumValues allDirs . normalize
+
+sumValues :: (Ord k, Num v) => [k] -> Map k v -> v
+sumValues keys mappy = map (\key -> Map.findWithDefault 0 key mappy) keys & sum
 
 go :: Dir -> Pos -> Pos
 go dir = Map.insertWith (+) dir 1
-
-distanceFromOrigin :: Pos -> Int
-distanceFromOrigin pos = north + northwest + northeast
-  where north = abs (pos ! N - pos ! S)
-        northeast = abs (pos ! NE - pos ! SW)
-        northwest = abs (pos ! NW - pos ! SE)
 
 followPath :: Pos -> [Dir] -> Pos
 followPath = foldr go
--- test/test11.hs
+++ test/test11.hs
@@ -3,13 +3,70 @@ import Test.Hspec
 import Test.QuickCheck
 import Lib
 import Hex
+import Debug.Trace
+
+import qualified Data.Map as Map
+import Data.Map(Map)
+
+expectValue :: Dir -> Map Dir Int -> Int -> ()
+expectValue = undefined
 
 main :: IO ()
 main = hspec $ do
+  describe "Cancel" $ do
+    it "is a no-op on a straight line" $ do
+      let newPos = followPath origin [S, S, S]
+      let cancelled = cancel N S newPos
+      cancel N S newPos `shouldBe` newPos
+      Map.lookup SE cancelled `shouldBe` Nothing
+      Map.lookup SW cancelled `shouldBe` Nothing
+      Map.lookup  N cancelled `shouldBe` Nothing
+      Map.lookup  S cancelled `shouldBe` Just 3
+
+    it "is a no-op when it misses the line" $ do
+      let newPos = followPath origin [SE, SE, SE]
+      cancel N S newPos `shouldBe` newPos
+
+  describe "Transmute" $ do
+    it "converts big numbers to a zero and a nonzero" $ do
+      let newPos = followPath origin [S, SE, SE, SW, SW, SW, SE, SW]
+      let transmuted = transmute S SE SW newPos
+      Map.lookup SE transmuted `shouldBe` Just 0
+      Map.lookup SW transmuted `shouldBe` Just 1
+      Map.lookup  S transmuted `shouldBe` Just 4
+
+    it "is a no-op if a source is zero" $ do
+      let newPos = followPath origin [SE, SE, SE]
+      let transmuted = transmute S SE SW newPos
+      Map.lookup SE transmuted `shouldBe` Just 3
+      Map.lookup SW transmuted `shouldBe` Nothing
+      Map.lookup  S transmuted `shouldBe` Just 0
+
+  describe "Normalize" $ do
+    it "is a no-op on a straight line" $ do
+      let newPos = followPath origin [SE, SE, SE]
+      let normalized = normalize newPos
+      Map.lookup SE normalized `shouldBe` Just 3
+      Map.lookup SW normalized `shouldBe` Just 0
+      Map.lookup  S normalized `shouldBe` Just 0
+      Map.lookup NW normalized `shouldBe` Just 0
+      Map.lookup NE normalized `shouldBe` Just 0
+      Map.lookup  N normalized `shouldBe` Just 0
+
+    it "cancels all directions" $ do
+      let newPos = followPath origin [N, NE, NW, SE, SW, S]
+      let normalized = normalize newPos
+      Map.lookup SE normalized `shouldBe` Just 0
+      Map.lookup SW normalized `shouldBe` Just 0
+      Map.lookup  S normalized `shouldBe` Just 0
+      Map.lookup NW normalized `shouldBe` Just 0
+      Map.lookup NE normalized `shouldBe` Just 0
+      Map.lookup  N normalized `shouldBe` Just 0
+
   describe "Distance from origin" $ do
     it "understands a straight line" $ do
       let newPos = followPath origin [SE, SE, SE]
-      distanceFromOrigin newPos `shouldBe` 3
+      trace (show newPos) $ distanceFromOrigin newPos `shouldBe` 3
 
     it "understands a southern zigzag" $ do
       let newPos = followPath origin [SE, SW, SE, SW]
@@ -34,3 +91,15 @@ main = hspec $ do
     it "cancels out opposite directions indirectly" $ do
       let newPos = followPath origin [N, N, N, SW, SE, SW]
       distanceFromOrigin newPos `shouldBe` 2
+
+    it "satisfies the samples given in the question" $ do
+      runTests distanceFromOrigin [
+        ([NE, NE, NE], 3),
+        ([NE, NE, SW, SW], 0),
+        ([NE, NE, S, S], 2),
+        ([SE, SW, SE, SW, SW], 3)
+        ]
+
+runTests :: (Eq a, Show a) => (Pos -> a) -> [([Dir], a)] -> IO ()
+runTests func datums = mapM_ checkEntry datums
+  where checkEntry (dirs, expected) = func (followPath origin dirs) `shouldBe` expected
