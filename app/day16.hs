c70b5b71bcd64be59821208041c8fb171595264f
Corey Knight <corey.knight@learnosity.com>
2018-01-02 22:27:05 +1100

    noncompiling errors in day16 as we change it to Vector for performance

--- app/day16.hs
+++ app/day16.hs
@@ -1,25 +1,42 @@
 
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
+
 import Text.ParserCombinators.ReadP
 import Data.Char
 import Data.List
 import Lib
+import qualified Data.Vector.Unboxed.Mutable as M
+import qualified Data.Vector.Unboxed as V
+import Control.Monad.ST
 
-starter = "abcdefghijklmnop"
+starter :: V.Vector Char
+starter = V.fromList "abcdefghijklmnop"
+
+type Instruction s = M.MVector s Char -> ST s ()
 
 main :: IO ()
 main = do
   input <- readFile "inputs/input16.txt" <&> lines <&> head
   let instructions = actuallyParse parseDance input
-  let result = foldl' (&) starter instructions
-  putStrLn result
-  let result2 = starter & iterate (\initial -> foldl' (&) initial instructions) & take 5
-  print result2
-  let rearrange = rearrangeVia (findIndexes result)
-  let result3 = result & iterate rearrange & take 5
-  print result3
-  print (rearrange starter)
+  let buffer = V.thaw starter
+  let result = buffer >>= applyInstructions instructions
+  let output = V.freeze buffer
+  print result
+  --let result2 = starter & iterate (\initial -> foldl' (&) initial instructions) & take 5
+  --print result2
+  putStrLn "a"
 
 -- "abcd" "bdca" -> [!! 1, !! 3, !! 2, !! 0]
+
+doInstructions :: [Instruction s] -> V.Vector Char -> V.Vector Char
+doInstructions instructions input = runST $ do
+  buffer <- V.thaw input
+  applyInstructions instructions buffer
+  V.freeze buffer
+
+applyInstructions :: [Instruction s] -> M.MVector s Char -> ST s ()
+applyInstructions instructions vector = map ($ vector) instructions & sequence_
 
 findIndexes :: String -> [Int]
 findIndexes string
@@ -27,45 +44,38 @@ findIndexes string
   & map ord
   & map (subtract (ord 'a'))
 
-rearrangeVia :: [Int] -> String -> String
-rearrangeVia indexes string
-  = map (string !!) indexes
+spin :: Int -> Instruction s
+spin num string = do
+  temp <- M.clone string
+  M.copy (M.slice 0 num string) (M.slice (M.length temp - num) num temp)
+  M.copy (M.slice num (M.length string - num) string) (M.slice 0 (M.length temp - num) temp)
 
-spin :: Int -> String -> String
-spin num string = drop (length string - num) string ++ take (length string - num) string
+exchange :: Int -> Int -> Instruction s
+exchange index1 index2 string = M.swap string index1 index2
 
-exchange :: Int -> Int -> String -> String
-exchange index1 index2 string
-  | index1 == index2 = string
-  | index1 > index2 = exchange index2 index1 string
-  | otherwise = prelude ++ [char2] ++ intermission ++ [char1] ++ finale
-    where char1 = string !! index1
-          char2 = string !! index2
-          prelude = take index1 string
-          intermission = take (index2 - index1 - 1) (drop (index1 + 1) string)
-          finale = drop (index2 + 1) string
+partner :: Char -> Char -> Instruction s
+partner char1 char2 string
+  = mapM_ doThing ([0..n] :: [Int])
+  where n = M.length string - 1 :: Int
+        doThing = M.modify string maybeSwap
+        maybeSwap c
+          | c == char1 = char2
+          | c == char2 = char1
+          | otherwise = c
 
-partner :: Char -> Char -> String -> String
-partner _ _ [] = []
-partner char1 char2 (c:string)
-  | c == char1 = char2 : next
-  | c == char2 = char1 : next
-  | otherwise = c : next
-  where next = partner char1 char2 string
-
-parseDance :: ReadP [String -> String]
+parseDance :: ReadP [Instruction s]
 parseDance = sepBy parseInstruction (char ',')
 
-parseInstruction :: ReadP (String -> String)
+parseInstruction :: ReadP (Instruction s)
 parseInstruction = parseSpin <++ parseExchange <++ parsePartner
 
-parseSpin :: ReadP (String -> String)
+parseSpin :: ReadP (Instruction s)
 parseSpin = do
   char 's'
   num <- parseInt
   pure $ spin num
 
-parseExchange :: ReadP (String -> String)
+parseExchange :: ReadP (Instruction s)
 parseExchange = do
   char 'x'
   index1 <- parseInt
@@ -73,7 +83,7 @@ parseExchange = do
   index2 <- parseInt
   pure $ exchange index1 index2
 
-parsePartner :: ReadP (String -> String)
+parsePartner :: ReadP (Instruction s)
 parsePartner = do
   char 'p'
   char1 <- get
