2f386ae5614e265929bb9cfdb37569a79b6102e0
Corey Knight <corey.knight@learnosity.com>
2018-01-17 14:37:14 +1100

    day22 part 1 is solved (with a good fleet of tests)
    then i started refactoring for day22 and something went wrong, so the
    tests are currently failing for part 1

--- app/day22.hs
+++ app/day22.hs
@@ -1,3 +1,11 @@
 
+import Day22Grid
+import Lib
+
 main :: IO ()
-main = undefined
+main = do
+  poses <- readFile "inputs/input22.txt" <&> inputToPoses
+  let status = initState poses
+  iterate update status & (!! 10000) & sInfectionEvents & length & print
+  putStrLn "5353 is too low"
+
--- src/Day22Grid.hs
+++ src/Day22Grid.hs
@@ -1,12 +1,13 @@
 
 module Day22Grid where
 
-import qualified Data.Set as Set
-import Data.Set(Set)
+import qualified Data.Map as Map
+import Data.Map(Map, (!))
 import Lib
 
 data Pos = Pos Int Int deriving (Show, Eq, Ord)
-data Dir = North | East | South | West deriving Show
+data Dir = North | East | South | West deriving (Show, Eq)
+data Health = Clean | Weakened | Infected | Flagged deriving (Show, Eq)
 
 clockwise :: Dir -> Dir
 clockwise North = East
@@ -22,39 +23,66 @@ counterclockwise East = North
 
 advance :: Dir -> Pos -> Pos
 advance East  (Pos x y) = Pos (x + 1) y
-advance North (Pos x y) = Pos x (y + 1)
+advance North (Pos x y) = Pos x (y - 1)
 advance West  (Pos x y) = Pos (x - 1) y
-advance South (Pos x y) = Pos x (y - 1)
+advance South (Pos x y) = Pos x (y + 1)
 
 data Dude = Dude {
                  dudePos :: Pos,
                  dudeDirection :: Dir
-                 } deriving Show
+                 } deriving (Show, Eq)
 
 data Status = Status {
-                dude :: Dude,
-                sInfections :: Set Pos,
+                sDude :: Dude,
+                sInfections :: Map Pos Health,
                 sInfectionEvents :: [Pos]
                 } deriving Show
 
 initState :: [Pos] -> Status
-initState poses = Status (Dude (Pos 0 0) North) (Set.fromList poses) []
+initState poses
+  = Status (Dude (Pos 0 0) North) infections []
+  where infections = zip poses (repeat Infected) & Map.fromList
 
 update :: Status -> Status
 update status@(Status dude infections infectionEvents)
-  = let pos = dudePos dude
-    in if Set.member pos infections
-      then clearInfection pos status & updateDude False
-      else createInfection pos status & updateDude True
+  = (updateHealth newHealth pos . updateDude directionChange) status
+  where pos = dudePos dude
+        (newHealth, directionChange) = case infections ! pos of
+                                        Clean -> (Infected, counterclockwise)
+                                        Infected -> (Clean, clockwise)
 
-clearInfection :: Pos -> Status -> Status
-clearInfection pos status = status { sInfections = Set.delete pos (sInfections status) }
+-- this is the "update" function for part 2 -- with weakened and flagged nodes
+reconstitute :: Status -> Status
+reconstitute status@(Status dude infections infectionEvents)
+  = (updateHealth newHealth pos . updateDude directionChange) status
+  where pos = dudePos dude
+        (newHealth, directionChange) = case infections ! pos of
+                                        Clean -> (Weakened, counterclockwise)
+                                        Weakened -> (Infected, id)
+                                        Infected -> (Flagged, clockwise)
+                                        Flagged -> (Clean, clockwise . clockwise)
 
-createInfection :: Pos -> Status -> Status
-createInfection pos status = status { sInfections = Set.insert pos (sInfections status), sInfectionEvents = pos : sInfectionEvents status }
+updateHealth :: Health -> Pos -> Status -> Status
+updateHealth health pos status
+  = status { sInfections = Map.insert pos health (sInfections status) }
 
-updateDude :: Bool -> Status -> Status
-updateDude isInfecting status = status { dude = Dude newPos newDirection }
-  where newDirection = if isInfecting then counterclockwise oldDirection else clockwise oldDirection
+updateDude :: (Dir -> Dir) -> Status -> Status
+updateDude directionChange status = status { sDude = Dude newPos newDirection }
+  where newDirection = directionChange newDirection
         newPos = advance newDirection oldPos
-        (Dude oldPos oldDirection) = dude status
+        (Dude oldPos oldDirection) = sDude status
+
+inputToPoses :: String -> [Pos]
+inputToPoses input
+  = zipWith (inscribeRow xMax yMax) [0..] values
+  & concat
+  where xMax = length (head inputs) `div` 2 -- assume every line has the same length
+        yMax = length inputs `div` 2
+        inputs = lines input
+        values = map (map (== '#')) inputs
+
+inscribeRow :: Int -> Int -> Int -> [Bool] -> [Pos]
+inscribeRow xOffset yOffset y bools
+  = zipWith (\x value -> (Pos (x - xOffset) (y - yOffset), value)) [0..] bools
+  & filter snd
+  & map fst
